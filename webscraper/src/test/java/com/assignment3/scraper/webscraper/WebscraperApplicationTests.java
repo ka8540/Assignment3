9723929324d9b9232d1a83ac3fb5f0bf7dca4dd2
ka8540 <ka8540@g.rit.edu>
2024-10-30 17:54:01 -0400

    basic structure ready

---
+++ webscraper/src/test/java/com/assignment3/scraper/webscraper/WebscraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment3.scraper.webscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebscraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
